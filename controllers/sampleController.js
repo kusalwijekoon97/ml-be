9d8c2c7f885d5997468383b4ee27991cdc634247
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-06-01 12:31:34 +0530

    auth

--- controllers/sampleController.js
+++ controllers/sampleController.js
@@ -1,14 +1,11 @@
 // sampleController.js
 exports.home = (req, res) => {
-    res.send('Hello from the controller!');
+  res.send("Hello from the controller!");
 };
 
 exports.getItems = (req, res) => {
-    // Logic to get items
-    res.json({ message: 'Get items' });
+  res.json({ message: "Get items" });
 };
 
-exports.createItem = (req, res) => {
-    // Logic to create an item
-    res.json({ message: 'Create item' });
-};
+exports.createItem = (req, res) => {};
+res.json({ message: "Create item" });
