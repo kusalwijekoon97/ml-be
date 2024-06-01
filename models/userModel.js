6c901cfd3471982673cbe32fc7565d832b6a7656
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-06-02 01:26:22 +0530

    crud done

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,15 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const users = [
   {
     id: 1,
-    username: 'user1',
-    password: bcrypt.hashSync('password1', 10),
+    username: "user1",
+    password: bcrypt.hashSync("password1", 10),
   },
   {
     id: 2,
-    username: 'user2',
-    password: bcrypt.hashSync('password2', 10),
+    username: "user2",
+    password: bcrypt.hashSync("password2", 10),
   }
 ];
 
--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const users = require('../../models/userModel');
-require('dotenv').config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const users = require("../../models/userModel");
+require("dotenv").config();
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   const user = users.find(u => u.username === username);
-  if (!user) return res.status(400).send('Cannot find user');
+  if (!user) return res.status(400).send("Cannot find user");
 
   if (!bcrypt.compareSync(password, user.password)) {
-    return res.status(403).send('Incorrect password');
+    return res.status(403).send("Incorrect password");
   }
 
-  const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
   res.json({ accessToken });
 });
 
