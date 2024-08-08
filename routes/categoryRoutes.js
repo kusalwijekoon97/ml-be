bccb916ae5021d4af19fa46d99fc2899e7b82737
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-08-08 21:47:05 +0530

    sub

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -13,13 +13,5 @@ router.post("/main/update/:id", categoryController.updateCategory);
 router.post("/main/delete/:id", categoryController.deleteCategory);
 router.post("/main/change-status/:id", categoryController.changeStatusCategory);
 
-// sub category
-router.post("/sub/store", categoryController.storeSubCategory);
-router.get("/sub/all", categoryController.getAllSubCategories);
-router.get("/sub/:id", categoryController.getSingleSubCategory);
-router.get("/sub/search", categoryController.getSearchedSubCategories);
-router.post("/sub/update/:id", categoryController.updateSubCategory);
-router.post("/sub/delete/:id", categoryController.deleteSubCategory);
-
 module.exports = router;
 
