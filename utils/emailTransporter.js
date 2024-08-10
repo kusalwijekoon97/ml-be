870634f058086655c1c69e203642924d6dfe1d39
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-08-10 23:36:28 +0530

    email donw

--- utils/emailTransporter.js
+++ utils/emailTransporter.js
@@ -2,9 +2,10 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-// Create a transporter
+// Create a transporter for Mailtrap
 let emailTransporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
