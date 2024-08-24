8531cd3494e5d64b0a239e75c9456128c9bd4823
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-08-24 16:31:30 +0530

    all open authors

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -6,10 +6,10 @@ const router = express.Router();
 
 router.post("/store", upload.single('profileImage'), authorController.storeAuthor);
 router.get("/all", authorController.getAllAuthors);
+router.get("/all-open", authorController.getOpenAllAuthors);
 router.get("/:id", authorController.showAuthor);
 router.post("/update/:id", upload.single('profileImage'), authorController.updateAuthor);
 router.post("/delete/:id", authorController.deleteAuthor);
 router.post("/change-status/:id", authorController.changeStatusAuthor);
-router.get("/all-open", authorController.getOpenAllAuthors);
 
 module.exports = router;
