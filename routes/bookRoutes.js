1bb9a60067b2888d1d948daa9d0f5114cd64861a
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-09-01 05:02:06 +0530

    conflict solved

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// routes\bookRoutes.js
-const express = require("express");
-const multerForBooks = require("../utils/multerForBooks"); 
-const bookController = require("../controllers/bookController");
-const router = express.Router();
-
-router.post("/store", multerForBooks.fields([
-    { name: 'coverImage', maxCount: 1 },
-    { name: 'additionalImages', maxCount: 5 },
-    { name: 'materialSources', maxCount: 10 } 
-]), bookController.storeBook);
-
-router.get("/", bookController.getAllBooks);
-router.get("/:id", bookController.getBookById);
-router.put("/:id", bookController.updateBook);
-router.delete("/:id", bookController.deleteBook);
-=======
 // routes/bookRoutes.js
 
 const express = require("express");
@@ -25,26 +7,11 @@ const bookController = require("../controllers/bookController");
 const router = express.Router();
 
 // Define multer fields configuration based on the frontend form structure
-<<<<<<< HEAD
-router.post("/store", upload.fields([
-    { name: 'coverImage', maxCount: 1 },
-    { name: 'material[completeMaterials][0][source]', maxCount: 1 },
-    { name: 'material[completeMaterials][1][source]', maxCount: 1 },
-    { name: 'material[completeMaterials][2][source]', maxCount: 1 },
-    { name: 'material[completeMaterials][3][source]', maxCount: 1 },
-    // { name: 'material[chapters][][chapter_source_pdf]', maxCount: 10 },
-    // { name: 'material[chapters][][chapter_source_epub]', maxCount: 10 },
-    // { name: 'material[chapters][][chapter_source_text]', maxCount: 10 },
-    // { name: 'material[chapters][][chapter_source_mp3]', maxCount: 10 }
-]), bookController.storeBook);
->>>>>>> dev-new-05_31_2024
-=======
 router.post("/store", upload.single('coverImage'), bookController.storeBook);
 router.get("/all", bookController.getAllBooks);
 router.get("/:id", bookController.showBook);
 // router.post("/update/:id", upload.single('fileBook'), bookController.updateBook);
 router.post("/delete/:id", bookController.deleteBook);
 router.post("/change-status/:id", bookController.changeStatusBook);
->>>>>>> dev-new-05_31_2024
 
 module.exports = router;
