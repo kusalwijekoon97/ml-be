6a222bf0880e5f5c7af49e2c1d9d5f90e1e39fb6
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-09-01 00:47:17 +0530

    all open materials for books

--- routes/materialRoutes.js
+++ routes/materialRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/store", upload.single('fileMaterial'), materialController.storeMaterial);
 router.get("/all", materialController.getAllMaterials);
+router.get("/all-open", materialController.getOpenAllMaterials);
 router.get("/:id", materialController.showMaterial);
 router.post("/update/:id", upload.single('fileMaterial'), materialController.updateMaterial);
 router.post("/delete/:id", materialController.deleteMaterial);
